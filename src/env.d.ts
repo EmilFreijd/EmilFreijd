a3753ad0fbb546373854ea953e9115d42d5f3b87
EmilFreijd <emil@freijd.com>
2026-07-12 07:27:55 +0200

    Revert "Improve editorial UI and mobile experience (#95)"
    This reverts commit 1f756fb28bfce3d3209ff1972dc6260258d853b1.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,3 @@
 declare module '@formspree/ajax/dist/index.mjs' {
   export * from '@formspree/ajax';
 }
-
-declare module '@fontsource-variable/inter/index.css';
-declare module '@fontsource-variable/newsreader/index.css';
-declare module '@fontsource-variable/newsreader/wght-italic.css';
